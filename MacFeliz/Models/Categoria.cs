8611c888dfd6ddbd256de67ac91b1949dbb27c14
gabs1san <sanbiel398@gmail.com>
2025-08-26 18:39:34 -0300

    Aplicando o Migrations e alterando a string de conexão

--- MacFeliz/Models/Categoria.cs
+++ MacFeliz/Models/Categoria.cs
@@ -15,7 +15,7 @@ namespace MacFeliz.Models
         public  string CategoriaNome { get; set; }
         [StringLength(200, ErrorMessage = "O tamanho máximo é 200 caracteres")]
         [Required(ErrorMessage = "Informe a descrão da categoria")]
-        [Display(Name = "Nome")]
+        [Display(Name = "Descrição")]
         public string Descricao { get; set; }
 
         public List<Lanche> Lanches { get; set; }
