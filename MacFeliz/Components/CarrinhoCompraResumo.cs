e7671244167e26f9c2d4df4185f07ed24f52bdf4
gabs1san <sanbiel398@gmail.com>
2025-09-10 19:45:53 -0300

    Criando a viewComponent

---
+++ MacFeliz/Components/CarrinhoCompraResumo.cs
@@ -0,0 +1,30 @@
+﻿using MacFeliz.Models;
+using MacFeliz.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MacFeliz.Components
+{
+    public class CarrinhoCompraResumo : ViewComponent
+    {
+        private readonly CarrinhoCompra _carrinhoCompra;
+
+        public CarrinhoCompraResumo(CarrinhoCompra carrinhoCompra)
+        {
+            _carrinhoCompra = carrinhoCompra;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var itens = _carrinhoCompra.GetCarrinhoCompraItens();
+            _carrinhoCompra.CarrinhoCompraItems = itens;
+
+            var carrinhoCompraVM = new CarrinhoCompraViewModel
+            {
+                CarrinhoCompra = _carrinhoCompra,
+                CarrinhoCompraTotal = _carrinhoCompra.GetCarrinhoCompraTotal()
+            };
+
+            return View(carrinhoCompraVM);
+        }
+    }
+}
