4b5824ebd0085683d43eb7d3c1a3fbd608006dd6
gabs1san <sanbiel398@gmail.com>
2025-09-10 19:50:56 -0300

    Teste do ícone

--- MacFeliz/Components/CarrinhoCompraResumo.cs
+++ MacFeliz/Components/CarrinhoCompraResumo.cs
@@ -15,7 +15,13 @@ namespace MacFeliz.Components
 
         public IViewComponentResult Invoke()
         {
-            var itens = _carrinhoCompra.GetCarrinhoCompraItens();
+            //var itens = _carrinhoCompra.GetCarrinhoCompraItens();
+
+            var itens = new List<CarrinhoCompraItem>()
+            {
+                new CarrinhoCompraItem(), new CarrinhoCompraItem()
+            };
+
             _carrinhoCompra.CarrinhoCompraItems = itens;
 
             var carrinhoCompraVM = new CarrinhoCompraViewModel
