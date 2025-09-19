fe971fd0c9ff50235d6880791b7110240136f333
gabs1san <sanbiel398@gmail.com>
2025-09-19 19:48:48 -0300

    Menu de categorias

---
+++ MacFeliz/Components/CategoriaMenu.cs
@@ -0,0 +1,21 @@
+﻿using MacFeliz.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MacFeliz.Views.Shared.Components
+{
+    public class CategoriaMenu : ViewComponent
+    {
+        private readonly ICategoriaRepository _categoriarepository;
+
+        public CategoriaMenu(ICategoriaRepository categoriarepository)
+        {
+            _categoriarepository = categoriarepository;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var categorias = _categoriarepository.Categorias.OrderBy(c => c.CategoriaNome);
+            return View(categorias);
+        }
+    }
+}
