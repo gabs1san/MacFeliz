df79ecd2b6a89875946629a6843c6084c81103fb
gabs1san <sanbiel398@gmail.com>
2025-11-18 20:38:49 -0300

    Implementando o Login

---
+++ MacFeliz/Controllers/AccountController.cs
@@ -0,0 +1,52 @@
+﻿using MacFeliz.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MacFeliz.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        
+        public IActionResult Login(string returnUrl)
+        {
+            return View(new LoginViewModel()
+            {
+                ReturnUrl = returnUrl
+            });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel loginVM)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(loginVM);
+            }
+
+            var user = await _userManager.FindByNameAsync(loginVM.UserName);
+            if(user != null)
+            {
+                var result = await _signInManager.PasswordSignInAsync(user, loginVM.Password, false, false);
+                if(result.Succeeded)
+                {
+                    if(!string.IsNullOrEmpty(loginVM.ReturnUrl))
+                    {
+                        return Redirect(loginVM.ReturnUrl);
+                    }
+                      return RedirectToAction("Index", "Home");
+                }
+            }
+            ModelState.AddModelError(string.Empty, "Nome de usuário ou senha inválidos");
+            return View(loginVM);
+        }
+    }
+}
