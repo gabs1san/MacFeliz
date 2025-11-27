539f9675d0f6d5ea828d229ab87e394c2dfd62db
gabs1san <sanbiel398@gmail.com>
2025-11-27 20:42:24 -0300

    Implemantando o registro

--- MacFeliz/Controllers/AccountController.cs
+++ MacFeliz/Controllers/AccountController.cs
@@ -15,7 +15,7 @@ namespace MacFeliz.Controllers
             _signInManager = signInManager;
         }
 
-        
+
         public IActionResult Login(string returnUrl)
         {
             return View(new LoginViewModel()
@@ -27,26 +27,51 @@ namespace MacFeliz.Controllers
         [HttpPost]
         public async Task<IActionResult> Login(LoginViewModel loginVM)
         {
-            if(!ModelState.IsValid)
+            if (!ModelState.IsValid)
             {
                 return View(loginVM);
             }
 
             var user = await _userManager.FindByNameAsync(loginVM.UserName);
-            if(user != null)
+            if (user != null)
             {
                 var result = await _signInManager.PasswordSignInAsync(user, loginVM.Password, false, false);
-                if(result.Succeeded)
+                if (result.Succeeded)
                 {
-                    if(!string.IsNullOrEmpty(loginVM.ReturnUrl))
+                    if (!string.IsNullOrEmpty(loginVM.ReturnUrl))
                     {
                         return Redirect(loginVM.ReturnUrl);
                     }
-                      return RedirectToAction("Index", "Home");
+                    return RedirectToAction("Index", "Home");
                 }
             }
             ModelState.AddModelError(string.Empty, "Nome de usuário ou senha inválidos");
             return View(loginVM);
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(LoginViewModel registroVM)
+        {
+            var user = new IdentityUser { UserName = registroVM.UserName };
+            var result = await _userManager.CreateAsync(user, registroVM.Password);
+
+            if(result.Succeeded)
+            {
+               await _signInManager.SignInAsync(user, isPersistent: false);
+                return RedirectToAction("Login", "Account");
+            }
+            else
+            {
+                this.ModelState.AddModelError("Registro", "falha ao registrar o usuário");
+            }
+
+            return View(registroVM);
         }
     }
 }
